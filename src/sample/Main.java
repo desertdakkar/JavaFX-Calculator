24b7149c2cea57ba4021f0d4ff41ecb8c7a4f647
desertdakkar <trovacerca@yandex.ru>
2017-01-07 11:46:31 +0700

    JavaFX Calculator

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,17 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.net.URL;
+
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../resources/sample.fxml"));
+        primaryStage.getIcons().add(new Image(getResource("../resources/icon.png").toExternalForm()));
         primaryStage.setTitle("Calculator");
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 370, 380));
         primaryStage.show();
+    }
+
+    private URL getResource(String name) {
+        return getClass().getResource(name);
     }
 
     public static void main(String[] args) {
